cce965c3ce269154facf9823a679a42db57744d1
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-02-10 14:11:22 +0000

    condicionales

---
+++ clase 2 2025-1/variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main (String[] args) {
+        int edad = 25;
+        double altura = 1.75;
+        String nombre = " Juan ";
+        boolean estudiante = true ;
+        System . out . println (" Nombre :␣" + nombre + ",␣ Edad :␣"
+        + edad + ",␣ Altura :␣" + altura + ",␣ Estudiante :␣"
+        + estudiante ) ;
+
+    }
+}
