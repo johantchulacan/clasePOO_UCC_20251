49608f5f528af15634c65b6204d955f6a0c6bdb9
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-02-10 14:12:08 +0000

    condicionales

---
+++ clase 2 2025-1/condicionales.java
@@ -0,0 +1,12 @@
+public class condicionales {
+    public static void main(String[] args) {
+        int numero = -5;
+        if (numero > 0){
+            System.out.println("el numero es positivo");
+        }else if (numero < 0){
+            System.out.println("el numero es negativo");
+        }else {
+            System.out.println("el numero es cero");
+        }
+    }
+}
