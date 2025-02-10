849a286d4b1af035aae2b40eac7bb4e00c1ae30e
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-02-10 15:10:11 +0000

    bucles

---
+++ clase 2 2025-1/bucles.java
@@ -0,0 +1,19 @@
+public class bucles {
+    public static void main (String[] args){
+
+        //for 
+
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i + ": programacion orientada a objetos");
+        }
+
+        System.out.println("--------------------------------");
+
+        //while
+        int j = 1;
+        while(j <= 5){
+            System.out.println(j + ": POO");
+            j++;
+        }
+    }
+}
