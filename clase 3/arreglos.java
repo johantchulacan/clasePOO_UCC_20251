44424cc6129ef802f591caebfe21fa252d2b5f73
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-02-17 13:45:13 +0000

    arreglos 2

--- clase 3/arreglos.java
+++ clase 3/arreglos.java
@@ -16,5 +16,20 @@ public class arreglos {
         for (int i= 0; i < a.length; i++) {
             System.out.println("a[" + i + "]=" + a [i]);
         }
+
+        //---------------------------------------------
+
+        // new instanciar , crear, darle vida al objeto
+
+        int tam = 10;
+        int[] b = new int[tam];
+
+        for(int i = 0; i < b.length; i++){
+            b[i] = (int) (Math.random() * (30 + 10)) + 10;
+        }
+
+        for (int i = 0; i < b.length; i++) {
+            System.out.println("b[" + i + "]=" + b[i]);
+        }
     }
 }
