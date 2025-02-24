973da2859a975ed66b023210c2b0c268b6deac94
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-02-24 14:16:35 +0000

    arrays

--- clase 3/ejercicio1.java
+++ clase 3/ejercicio1.java
@@ -16,25 +16,27 @@ public class ejercicio1 {
         }
         System.out.println("La suma es: " + suma);
 
- // Ejercicio 2: Neto a pagar por empleado
- System.out.print("Ingrese la cantidad de empleados: ");
- int empleados = scanner.nextInt();
- int[] sueldos = new int[empleados];
- int[] asignaciones = new int[empleados];
- int[] deducciones = new int[empleados];
- int[] netoPagar = new int[empleados];
- for (int i = 0; i < empleados; i++) {
-     System.out.print("Ingrese el sueldo del empleado " + (i + 1) + ": ");
-     sueldos[i] = scanner.nextInt();
-     System.out.print("Ingrese las asignaciones: ");
-     asignaciones[i] = scanner.nextInt();
-     System.out.print("Ingrese las deducciones: ");
-     deducciones[i] = scanner.nextInt();
-     netoPagar[i] = sueldos[i] + asignaciones[i] - deducciones[i];
- }
- System.out.println("Neto a pagar por empleado: ");
- for (int i = 0; i < empleados; i++) {
-     System.out.println("Empleado " + (i + 1) + ": " + netoPagar[i]);
- }
-}
+        // Ejercicio 2: Neto a pagar por empleado
+        System.out.print("Ingrese la cantidad de empleados: ");
+        int empleados = scanner.nextInt();
+        int[] sueldos = new int[empleados];
+        int[] asignaciones = new int[empleados];
+        int[] deducciones = new int[empleados];
+        int[] netoPagar = new int[empleados];
+        for (int i = 0; i < empleados; i++) {
+            System.out.print("Ingrese el sueldo del empleado " + (i + 1) + ": ");
+            sueldos[i] = scanner.nextInt();
+            System.out.print("Ingrese las asignaciones: ");
+            asignaciones[i] = scanner.nextInt();
+            System.out.print("Ingrese las deducciones: ");
+            deducciones[i] = scanner.nextInt();
+            netoPagar[i] = sueldos[i] + asignaciones[i] - deducciones[i];
+        }
+        System.out.println("Neto a pagar por empleado: ");
+        for (int i = 0; i < empleados; i++) {
+            System.out.println("Empleado " + (i + 1) + ": " + netoPagar[i]);
+        }
+
+        scanner.close();
+    }
 }
