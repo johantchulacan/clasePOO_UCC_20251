2453129facbaa16ba9a79d99e4cac44f63c74276
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-03-17 13:29:09 +0000

    constructor

--- momento2/Estudiante.java
+++ momento2/Estudiante.java
@@ -6,6 +6,18 @@ public class Estudiante {
     private int edad;
     private String facultad;
 
+    //constructor de la clase
+    //me permite inicializar la clase
+
+    public Estudiante(int id, String nombre, String apellido, int edad, String facultad){
+        //autorefencia  a los atributos de a clase
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.facultad = facultad;
+
+    }
     //metodos
     public void matricularCursos(String curso){
         System.out.println("curso: " + curso + " matriculado!!.");
